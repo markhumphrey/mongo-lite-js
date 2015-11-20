2e3eb0d79f8b344844e820c7b23d1a69a8352bf1
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-11-20 15:27:03 -0500

    Reorganize structure and add jasmine test support

---
+++ spec/mongo-client-spec.js
@@ -0,0 +1,19 @@
+var MongoClient = require("../lib/mongo-client");
+
+describe("connect", function() {
+
+  var client;
+  beforeEach(function() {
+    client = new MongoClient();
+  });
+
+  it("should create a memory db", function() {
+    expect(true);
+  });
+
+  it("should throw an error on null callback", function() {
+    expect(function() {
+      client.connect("", {}, null);
+    }).toThrow();
+  });
+});
